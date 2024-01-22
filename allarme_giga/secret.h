64adcb1cb861d3b4886bc94487765d9a58a4edb1
emiliano <emiliano4458@gmail.com>
2024-01-23 00:08:27 +0100

    adding new board and telegram bot

--- allarme_giga/secret.h
+++
@@ -1,7 +0,0 @@
-#define wifi_ssid "Casa Pedica"
-#define wifi_password "sandonato"
-#define telegram_token "1657272519:AAEW6Zu_7NzOpI1qph9VecZw1mrPx6-er-I"
-#define id_1 176562109 //id emiliano
-#define id_2 //id giuliano
-#define id_3 //id mamma
-#define id_4 //id papà
